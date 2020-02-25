0b8e9f6c858c46a91fffc0299c22a4420a20a9ee
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-02-25 03:36:57 -0600

    Add HDFS ingestion capability to main class
    - Now, the inputs for the IngestFiles class are parameterized as:
      - 1) The datasource URL.
      - 2) The landing folder for the resource.
      - 3) The file-system URI for identify the HDFS.
      - 4) The HDFS target folder.

--- src/main/scala/com/altimetrik/gobike/IngestFiles.scala
+++ src/main/scala/com/altimetrik/gobike/IngestFiles.scala
@@ -1,10 +1,17 @@
-import com.altimetrik.gobike.utils.IngestionUtils
+package com.altimetrik.gobike
+
+import com.altimetrik.gobike.utils.IngestionUtils._
 
 object IngestFiles extends App {
-	// Configuring landing zone target
-	val resourceUrl = "https://s3.amazonaws.com/fordgobike-data/2017-fordgobike-tripdata.csv"
-	// Edge node
-	val landingFolder = "landing"
-	IngestionUtils.fetchResourceFrom( resourceUrl, landingFolder, true )
+	// Fetching file from remote URL to landing zone
+	val resourceUrl = s"${args( 0 )}"
+	val landingFolder = s"${args( 1 )}"
+	val fileSystemUri = s"${args( 2 )}"
+	val hdfsTargetFolder = s"${args( 3 )}"
+	val fileToIngest = fetchResourceFrom( resourceUrl, landingFolder, true )
+	
+	// Ingesting file to HDFS target
+	ingestToHdfs( fileSystemUri, fileToIngest, hdfsTargetFolder )
+	
 	println( "Done!" )
 }
